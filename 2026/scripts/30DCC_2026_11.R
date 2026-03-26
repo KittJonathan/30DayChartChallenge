215505fa4e7e533b98cda93e7ddddbd35c992fe6
Jonathan Kitt <jonathan.kitt@inrae.fr>
2026-03-26 10:19:29 +0100

    #30DCC -> day 11

--- 2026/scripts/30DCC_2026_11.R
+++ 2026/scripts/30DCC_2026_11.R
@@ -6,6 +6,30 @@
 
 # 📦 Packages ----
 
+library(tidyverse)
+library(palmerpenguins)
+library(patchwork)
+
 # 📄 Data ----
 
 # 📊 Plot ----
+
+p1 <- penguins |> 
+  ggplot() +
+  geom_histogram(aes(x = bill_length_mm, fill = species))
+
+p2 <- penguins |> 
+  ggplot() +
+  geom_histogram(aes(x = bill_depth_mm, fill = species))
+
+p3 <- penguins |> 
+  ggplot() +
+  geom_histogram(aes(x = flipper_length_mm, fill = species))
+
+p4 <- penguins |> 
+  ggplot() +
+  geom_histogram(aes(x = body_mass_g, fill = species))
+
+p <- (p1 / p2 / p3 / p4)
+
+ggsave("2026/figs/30DCC_2026_11.png", p, dpi = 320, width = 12, height = 6)
