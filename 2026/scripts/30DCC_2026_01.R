2998a0ee1fa35db9a8ae1e890de16ba212c411e8
Jonathan Kitt <jonathan.kitt@inrae.fr>
2026-03-26 22:26:54 +0100

    30DCC

--- 2026/scripts/30DCC_2026_01.R
+++ 2026/scripts/30DCC_2026_01.R
@@ -7,78 +7,58 @@
 # 📦 Packages ----
 
 library(tidyverse)
-library(sysfonts)
-font_add_google("Roboto")
-font_add_google("Ubuntu")
+library(palmerpenguins)
+library(ggtext)
+library(showtext)
+
+# ⚙️ Define plot parameters ----
+
 font_add_google("Open Sans")
 showtext::showtext_auto()
 
-# 📄 Data ----
-
-# Source : Our World In Data - Electricity Mix
-# https://ourworldindata.org/electricity-mix
-
-elec_mix <- read_csv("2026/data/electricity-prod-source-stacked.csv")
-
-elec_df <- elec_mix |> 
-  filter(Entity == "World", Year == 2024) |> 
-  select(!(Entity:Year)) |> 
-  pivot_longer(everything(),
-               names_to = "Source",
-               values_to = "TWh") |> 
-  arrange(TWh) |> 
-  mutate(Source = fct_inorder(Source))
-
-elec_labels <- elec_mix |> 
-  filter(Entity == "World", Year == 2024) |> 
-  select(!(Entity:Year)) |> 
-  pivot_longer(everything(),
-               names_to = "Source",
-               values_to = "TWh") |> 
-  arrange(desc(TWh)) |> 
-  mutate(seg_start_x = 1.5,
-         seg_end_x = 1.75,
-         seg_start_y = cumsum(lag(TWh, default = 0)),
-         seg_start_y = seg_start_y + (TWh / 2),
-         seg_end_y = case_when(Source == "Bioenergy" ~ seg_start_y + 750,
-                               Source == "Other renewables" ~ seg_start_y + 1750,
-                               .default = seg_start_y),
-         label = paste0(Source, " (", round(TWh), " TWh)")
-  )
-
 # 📊 Plot ----
 
-my_cols <- c("Coal" = "#000000",
-             "Gas" = "#d55e00",
-             "Hydropower" = "#56b4e9",
-             "Nuclear" = "#e69f00",
-             "Wind" = "#cc79a7",
-             "Solar" = "#f0e442",
-             "Oil" = "#ef9877",
-             "Bioenergy" = "#335a21",
-             "Other renewables" = "#009e73")
-
-p <- ggplot() +
-  geom_col(data = elec_df, aes(x = 1, y = TWh, fill = Source),
-           position = "stack", show.legend = FALSE, width = 1) +
-  scale_fill_manual(values = my_cols) +
-  geom_segment(data = elec_labels,
-               aes(x = seg_start_x, xend = seg_end_x,
-                   y = seg_start_y, yend = seg_end_y),
-               linewidth = 0.3) +
-  geom_text(data = elec_labels,
-            aes(x = 1.76, y = seg_end_y, label = label),
-            hjust = 0, family = "Open Sans", size = 10) +
-  scale_x_continuous(limits = c(0.5, 2)) +
-  labs(title = "Electricity mix",
-       subtitle = "Worldwide - 2025",
-       caption = "30DayChartChallenge 2026 | Comparisons | Day 01 - Part-to-Whole | Source: Our World In Data") +
+p <- penguins |> 
+  count(island, name = "count") |> 
+  mutate(fraction = count / sum(count),
+         ymax = cumsum(fraction),
+         ymin = lag(ymax, default = 0),
+         label_y = (ymax + ymin) / 2) |> 
+  ggplot(aes(xmin = 3, xmax = 4,
+             ymin = ymin, ymax = ymax,
+             fill = island)) +
+  geom_rect(show.legend = FALSE,
+            color = "black") +
+  scale_fill_manual(
+    values = c("Biscoe" = "#816ec7",
+               "Dream" = "#e5989b",
+               "Torgersen" = "#ffcdb2")
+  ) +
+  geom_text(x = 3.5, aes(y = label_y, label = count),
+            size = 25, family = "Open Sans") +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
+  labs(title = "How many penguins?",
+       subtitle = "Number of individuals on each island: <span style = 'color:#816ec7;'>Biscoe</span>,
+       <span style = 'color:#e5989b;'>Dream</span>,
+       <span style = 'color:#ffcdb2;'>Torgersen</span>",
+       caption = "30DayChartChallenge 2026 | Comparisons | Day 01 - Part-to-Whole | Source: {palmerpenguins}") +
   theme_void() +
-  theme(panel.background = element_rect(fill = "white"),
-        plot.background = element_rect(fill = "white"),
-        plot.title = element_text(family = "Open Sans", face = "bold", size = 40, hjust = 0.5, margin = margin(t = 15)),
-        plot.subtitle = element_text(family = "Open Sans", size = 35, hjust = 0.5),
-        plot.caption = element_text(family = "Open Sans", size = 30, hjust = 0.5, margin = margin(b = 15)))
+  theme(
+    panel.background = element_rect(fill = "black", color = "black"),
+    plot.background = element_rect(fill = "black", color = "black"),
+    plot.title = element_text(family = "Open Sans", face = "bold", 
+                              colour = "white", size = 80, hjust = 0.5,
+                              margin = margin(t = 10)),
+    plot.subtitle = element_markdown(family = "Open Sans", 
+                                     colour = "white", size = 60, hjust = 0.5,
+                                     margin = margin(t = 10)),
+    plot.caption = element_text(family = "Open Sans", colour = "white",
+                                size = 40, hjust = 0.5, 
+                                margin = margin(b = 10))
+  )
 
-ggsave("2026/figs/30DCC_2026_01.png", p, dpi = 320, width = 12, height = 6)
+# 💾 Save plot ----
 
+ggsave("2026/figs/30DCC_2026_01.png", p, dpi = 320,
+       width = 12, height = 6)
--- 2026/scripts/30DCC_2026_02.R
+++ 2026/scripts/30DCC_2026_02.R
@@ -7,13 +7,16 @@
 # 📦 Packages ----
 
 library(tidyverse)
+library(palmerpenguins)
 library(ggpop)
-library(patchwork)
-library(sysfonts)
-font_add_google("Roboto")
-font_add_google("Ubuntu")
+# library(ggtext)
+library(showtext)
+
+# ⚙️ Define plot parameters ----
+
 font_add_google("Open Sans")
 showtext::showtext_auto()
+
 
 # 📄 Data ----
 
@@ -34,6 +37,33 @@ mobile <- read_csv("2026/data/share-population-mobile-phone.csv") |>
   mutate(icon = "mobile-screen")
 
 # 📊 Plot ----
+
+penguins |> 
+  count(species)
+
+tibble(
+  x = c(1:5),
+  y = c(6:10)
+) |> 
+  ggplot(aes(x, y)) +
+  geom_icon_point(icon = "linux", size = 3) 
+
+p <- tibble(
+  species = c(rep("Chinstrap", 68), rep("Gentoo", 124), 
+              rep("Adelie", 152)), 
+  x = c(rep(1:6, each = 10), rep(7, 8),
+        rep(1:12, each = 10), rep(13, 4),
+        rep(1:15, each = 10), rep(16, 2)),
+  y = c(rep(1:10, times = 6), 10:3,
+        rep(12:21, times = 12), 21:18,
+        rep(23:32, times = 15), 32, 31)
+) |> 
+  ggplot(aes(x, y, color = species)) +
+  geom_icon_point(icon = "linux", size = 1) +
+  theme(legend.position = "none")
+
+ggsave("2026/figs/30DCC_2026_02_draft.png", p, dpi = 320, width = 12, height = 6)
+
 
 p1 <- ggplot() +
   geom_text(aes(x = 0, y = 1.5, label = "Approximately"),
