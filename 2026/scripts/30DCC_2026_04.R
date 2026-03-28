d7ada3a69f39d628332a575bceab50718d432113
Jonathan Kitt <jonathan.kitt@inrae.fr>
2026-03-28 22:26:32 +0100

    30DCC day 4 ok

--- 2026/scripts/30DCC_2026_04.R
+++ 2026/scripts/30DCC_2026_04.R
@@ -8,7 +8,6 @@
 
 library(tidyverse)
 library(palmerpenguins)
-# library(marimekko)
 library(showtext)
 # library(ggtext)
 
@@ -31,18 +30,35 @@ p <- penguins |>
                "Chinstrap" = "purple",
                "Gentoo" = "cyan4")
   ) +
+  scale_fill_manual(
+    values = c("Adelie" = "darkorange",
+               "Chinstrap" = "purple",
+               "Gentoo" = "cyan4")
+  ) +
   facet_wrap(~species) +
-  labs(x = "Flipper length (mm)",
-       y = "Bill depth (mm)") +
+  labs(title = "Relation between flipper length and bill length",
+       x = "Flipper length (mm)",
+       y = "Bill length (mm)",
+       caption = "30DayChartChallenge 2026 | Comparisons | Day 04 - Slope | Source: {palmerpenguins}") +
   theme(legend.position = "none",
         panel.grid.minor = element_blank(),
         panel.grid.major = element_line(colour = "white", linetype = "dotted",
                                         linewidth = 0.2),
         panel.background = element_rect(fill = "black", colour = "black"),
         plot.background = element_rect(fill = "black", colour = "black"),
-        strip.background = element_rect(colour="white", fill="black", 
-                                        linewidth = 1.5, linetype = "solid"),
-        strip.text = element_text(family = "Open Sans", colour = "white", size = 50))
+        plot.title = element_text(family = "Open Sans", face = "bold", colour = "white", 
+                                  size = 80, hjust = 0.5, margin = margin(t = 10, b = 20)),
+        plot.caption = element_text(family = "Open Sans", colour = "white",
+                                    size = 40, hjust = 0.5, 
+                                    margin = margin(t = 20, b = 10)),
+        axis.title = element_text(family = "Open Sans", colour = "white", size = 40),
+        axis.text = element_text(family = "Open Sans", colour = "white", size = 34),
+        axis.text.x = element_text(margin = margin(t = 10, b = 10)),
+        axis.text.y = element_text(margin = margin(l = 10, r = 10)),
+        strip.background = element_rect(colour="white", fill="white"),
+        strip.text = element_text(family = "Open Sans", colour = "black", size = 50))
+
+p
 
 ggsave("2026/figs/30DCC_2026_04.png", p, dpi = 320, width = 12, height = 6) 
 
